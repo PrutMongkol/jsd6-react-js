262ef87236fd131c11d824eb102c06ded7423784
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2023-12-25 15:14:22 +0700

    finish react-1/exercise-3

--- react-1/exercise-3/src/App.jsx
+++ react-1/exercise-3/src/App.jsx
@@ -10,13 +10,38 @@ function App() {
 
 const RegistrationForm = () => {
   return (
-    // Code here
+    <div className="container">
+      <h1>Registration Form</h1>
+      <form id="registrationForm">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 };
 
 const DisplayMember = () => {
+  const adam = {
+    name: 'Adam Smith',
+    email: 'a.smith@example.com',
+  };
+  const jane = {
+    name: 'Jane Baker',
+    email: 'j.baker@example.com'
+  };
   return (
-    // Code here
+    <div className="members">
+      <h2>Registered Members</h2>
+      <div className="member">{adam.name} ({adam.email})</div>
+      <div className="member">{jane.name} ({jane.email})</div>
+    </div>
   );
 };
 
