6650c5f8f84b6fe95cc94f8b8b50efa2a9b8e80c
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2023-12-28 15:26:40 +0700

    react-4/exercise-2: first version

--- react-4/exercise-2/src/App.jsx
+++ react-4/exercise-2/src/App.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const peopleData = [
+  {
+      name: 'John',
+      organization: 'ABC Inc.',
+      position: 'Developer',
+  },
+  {
+      name: 'Jane',
+      organization: 'XYZ Corp.',
+      position: 'Designer',
+  },
+  {
+      name: 'Bob',
+      organization: '123 LLC',
+      position: 'Manager',
+  },
+];
 
 function App() {
+  const [people, setPeople] = useState(peopleData);
+  const [name, setName] = useState();
+  const [organization, setOrganization] = useState();
+  const [position, setPosition] = useState();
+
+  const repopulatePeople = () => {
+    setPeople(peopleData);
+  };
+
+  useEffect(repopulatePeople, []);
+
+  const handleName = ({ target }) => {
+    setName(target.value);
+  };
+
+  const handleOrganization = ({ target }) => {
+    setOrganization(target.value);
+  };
+
+  const handlePosition = ({ target }) => {
+    setPosition(target.value);
+  };
+
+  const handleSave = () => {
+    const newPerson = {
+      name: name,
+      organization: organization,
+      position: position,
+    }
+
+    setPeople((prevPeople) => [...prevPeople, newPerson]);
+
+    setName('');
+    setOrganization('');
+    setPosition('');
+  };
+
   return (
     <div className="App"> {/* Add class name */}
       <h1>Table Example</h1>
       <div>
-        <input type="text" placeholder="Type new name here" style={{margin: '4px'}} />
-        <input type="text" placeholder="Type new organization here" style={{margin: '4px'}} />
-        <input type="text" placeholder="Type new position here" style={{margin: '4px'}} />
-        <button style={{margin: '4px'}}>Save</button>
+        <input type="text" placeholder="Type new name here" style={{margin: '4px'}} value={name} onChange={handleName} />
+        <input type="text" placeholder="Type new organization here" style={{margin: '4px'}} value={organization} onChange={handleOrganization} />
+        <input type="text" placeholder="Type new position here" style={{margin: '4px'}} value={position} onChange={handlePosition} />
+        <button style={{margin: '4px'}} onClick={handleSave}>
+          Save
+        </button>
       </div>
 
       <table>
@@ -20,21 +77,17 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>John</td>
-            <td>ABC Inc.</td>
-            <td>Developer</td>
-          </tr>
-          <tr>
-            <td>Jane</td>
-            <td>XYZ Corp.</td>
-            <td>Designer</td>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>123 LLC</td>
-            <td>Manager</td>
-          </tr>
+          {
+            people.map((element) => {
+              return (
+                <tr>
+                  <td>{element.name}</td>
+                  <td>{element.organization}</td>
+                  <td>{element.position}</td>
+                </tr> 
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
---
+++ react-4/exercise-2/src/AutoSync.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const peopleData = [
+    {
+        name: 'John',
+        organization: 'ABC Inc.',
+        position: 'Developer',
+    },
+    {
+        name: 'Jane',
+        organization: 'XYZ Corp.',
+        position: 'Designer',
+    },
+    {
+        name: 'Bob',
+        organization: '123 LLC',
+        position: 'Manager',
+    },
+];
